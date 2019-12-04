bf3eb9a77b7f0bb6af4637b76a160268b35d85f6
Margaret Butler <punkydoodle@dhcp-10-250-180-37.harvard.edu>
2019-12-04 07:09:14 -0500

    single graphic and about page are up. moving to wordcloud work

--- gothiclit/app.R
+++ gothiclit/app.R
@@ -27,6 +27,8 @@ theme_set(theme_classic())
 
 
 sentiment_time <- read_rds("sentiment_time.rds")
+tidy_gothic <- read_rds("tidy_gothic.rds")
+messy_gothic <- read_rds("messy_gothic.rds")
 
 
 ### UI
@@ -37,9 +39,28 @@ ui <- fluidPage(
     theme = shinytheme("flatly"),
     
     navbarPage(
-        "Monstrosity of Language in Gothic Lit",
+        "Gothic Literature and Monstrosity",
         tabPanel(
-            title = "Introduction"
+            title = "About",
+            h5("By Margaret Butler"),
+            h3("Tracking the Monstrous in Five Gothic Novels"),
+            p(
+                "Monstrosity in literature, gothic lit in particular, tends to evoke specific images and draws on a very specific linguistic tradition. 
+                I was initially curious about tracking shared word usage and pronoun usage in classic gothic novels that investigate the relationship between 
+                humanity and monstrosity."
+            ),
+            br(),
+            p(
+              "Pronouns are used in books such as Mary Shelley's *Frankenstein* to connote a transition from a character's perception as human to monstrous. 
+              I was interested in seeing if this was a tradition one could track statistically in other books or if it was even a a traceable trend that five 
+              classic examples of monster literature followed."
+              ),
+            br(),
+            p(
+            "The first few graphics I have give visualization to words trends throughout the novels—more specifically, we get to see a visual representation 
+            of what exactly makes these novels “gothic”: the negative sentiment, the specific words used, and so on. This is the springboard into pronoun and 
+            monstrosity analysis of the greater question: can looking at word usage analytically help us identify negativity and monstrosity in literature?"
+              )
         ),
         tabPanel(
            "Sentiment Analysis",
@@ -64,13 +85,17 @@ ui <- fluidPage(
 server <- function(input, output) {
     output$sentintro <- renderUI({
         HTML(
-            "Yee haw"
+            "The following graphics represent positive over negative sentiment throughout the course of each book. 
+            As you can see if you poke around, sentiment in all the texts is overwhelmingly negative. This tracks 
+            with what one expects of gothic literature; in stories about death and monstrosity, negative sentiment 
+            really ought to be dominant. The question that emerges of this is: are books that have more negative 
+            sentiment more monstrous?"
         )
     })
     output$sentplot <- renderPlot({
         sentiment_time %>% 
             filter(title == input$title) %>% 
-            ggplot(aes(index, sentiment, fill = title)) +
+            ggplot(aes(index, sentiment, fill = sentiment)) +
                 geom_col(show.legend = FALSE)
     })
 
